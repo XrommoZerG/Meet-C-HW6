5ec725841c742fa4e1f787ea40d42e199b91d623
Artem <FxD1ag767@outlook.com>
2023-07-02 17:20:46 +0300

    Task 41 compleet

---
+++ task41/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int[] Vod(int lenght)
+{
+
+    int[] mass = new int[lenght];
+
+    for (int i = 0; i < lenght; i++)
+    {
+        Console.WriteLine($"Введите число {i}:");
+        mass[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return mass;
+}
+
+int Stav(int[] mass)
+{
+    int coll = 0;
+    for (int i = 0; i < mass.Length; i++)
+    {
+        if (mass[i] > 0)
+        {
+            coll++;
+        }
+    }
+    return coll;
+}
+Console.WriteLine("Сколько чисел вы ведёте - ");
+int lenght = Convert.ToInt32(Console.ReadLine());
+int[] array = Vod(lenght);
+string chis = " ";
+
+if (Stav(array) > 4 || Stav(array) == 0)
+{
+    chis = "чисел";
+}
+else if (Stav(array) == 1)
+{
+    chis = "число";
+}
+else
+{
+    chis = "числа";
+}
+
+Console.WriteLine($"Пользователь вёл {Stav(array)} чисел больше 0 ");
+
